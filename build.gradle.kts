53570af87a4e82cc3a0aa51b2e8adecfe4290d40
Mykhailo Vasylenko <mvasylenko@solvd.com>
2022-03-31 20:48:05 +0300

    extracted common android.gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,42 @@ buildscript{
     }
 }
 
-tasks.register("clean",Delete::class){
+
+subprojects {
+    afterEvaluate {
+        if (hasProperty("android")) {
+            android {
+                compileSdk = Dependency.AppConfig.compileSdkVersion
+
+                defaultConfig {
+                    minSdk = Dependency.AppConfig.minSdkVersion
+                    targetSdk = Dependency.AppConfig.targetSdkVersion
+                    testInstrumentationRunner = TestDependency.testRunner
+                }
+
+                compileOptions {
+                    sourceCompatibility(JavaVersion.VERSION_1_8)
+                    targetCompatibility(JavaVersion.VERSION_1_8)
+                }
+
+                kotlinOptions {
+                    jvmTarget = "1.8"
+                }
+
+                buildFeatures {
+                    dataBinding = true
+                }
+            }
+        }
+    }
+}
+
+tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }
+
+fun Project.android(configure: Action<com.android.build.gradle.internal.dsl.BaseAppModuleExtension>): Unit =
+    (this as ExtensionAware).extensions.configure("android", configure)
+
+fun com.android.build.gradle.internal.dsl.BaseAppModuleExtension.kotlinOptions(configure: Action<org.jetbrains.kotlin.gradle.dsl.KotlinJvmOptions>): Unit =
+    (this as ExtensionAware).extensions.configure("kotlinOptions", configure)
